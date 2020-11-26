b10401f3cc69ec18f2a9553cf1e33d28d764162a
unknown <you@example.com>
2020-11-26 13:19:52 +0530

    Added new file

--- UCuserinfo.sh
+++ UCuserinfo.sh
@@ -1,10 +1,12 @@
 #!/bin/bash -x
 shopt -s extglob
 read -p "Enter the password:" pass
-pat='^.{8,}$'
+pat='^(?=.*?[[:upper:]])(?=.*?[[:lower:]]).{8,}$'
 if [[ $pass =~ $pat ]] #+
 then
         echo "user password is valid"
+
 else
+
         echo " user password not valid"
 fi
