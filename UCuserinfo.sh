05995e642b43e1d6dcab3fedae657f2c90d2a9af
unknown <you@example.com>
2020-11-26 12:02:48 +0530

    Added password file

--- UCuserinfo.sh
+++ UCuserinfo.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -x
 shopt -s extglob
-read -p "Enter the mobile number  :" monum
-pat='^(91){1}[ ][1-9]{1}[0-9]{9}$'
-if [[ $monum =~ $pat ]] #+
+read -p "Enter the password:" pass
+pat='^.{8,}$'
+if [[ $pass =~ $pat ]] #+
 then
-        echo "user mobilenumber is valid"
+        echo "user password is valid"
 else
-        echo " user mobilenumber not valid"
+        echo " user password not valid"
 fi
