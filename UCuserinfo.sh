3daa119d652a946c8ba675720bba9fecb6fa42af
unknown <you@example.com>
2020-11-26 11:57:04 +0530

    Added new file of mobilenumber

--- UCuserinfo.sh
+++ UCuserinfo.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -x
 shopt -s extglob
-read -p "Enter the Email  :" Email
-pat='^[a-zA-Z0-9]+([.+-_][a-zA-Z0-9]+)*@[a-zA-Z0-9]+[.]{1}[a-z]{2,4}([.][a-z]{2})*$'
-if [[ $Email =~ $pat ]] #+
+read -p "Enter the mobile number  :" monum
+pat='^(91){1}[ ][1-9]{1}[0-9]{9}$'
+if [[ $monum =~ $pat ]] #+
 then
-        echo "user Email is valid"
+        echo "user mobilenumber is valid"
 else
-        echo " user Email not valid"
+        echo " user mobilenumber not valid"
 fi
