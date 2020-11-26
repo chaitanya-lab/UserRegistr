57d90f53d1127b3712c3ca9870546e764687a7ee
chaitanya-lab <74778485+chaitanya-lab@users.noreply.github.com>
2020-11-26 20:10:52 +0530

    Update UCuserinfo.sh

--- UCuserinfo.sh
+++ UCuserinfo.sh
@@ -1,6 +1,14 @@
 #!/bin/bash -x
 shopt -s extglob
-read -p "Enter the Email  :" lastname
+read -p "Enter the firstname  :" firstname
+pat='^[a-zA-Z]{3,}$'
+if [[ $firstname =~ $pat ]] #+
+then
+        echo "user firststname is valid"
+else
+        echo " user firstname not valid"
+fi
+read -p "Enter the laststname  :" laststtname
 pat='^[a-zA-Z]{3,}$'
 if [[ $lastname =~ $pat ]] #+
 then
