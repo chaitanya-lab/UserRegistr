9ad8d4635701b56f31f9b339f3529ffa0d1ae507
unknown <you@example.com>
2020-11-26 11:38:17 +0530

    New file

---
+++ UCuserinfo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -x
+shopt -s extglob
+read -p "Enter the Email  :" firstname
+pat='^[a-zA-Z]{3,}$'
+if [[ $firstname =~ $pat ]] #+
+then
+        echo "user name is valid"
+else
+        echo " user name not valid"
+fi
